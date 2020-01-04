268a5ac27f39c798af0777443fb44eeaa841bed6
Darryl <dazsnow@gmail.com>
2020-01-04 17:01:43 +0800

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,59 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   extends: [
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
   ],
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ["@typescript-eslint", "prettier"],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    sourceType: "module" // Allows for the use of imports
   },
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   settings: {
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
     },
-    'import/resolver': {
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
   },
-  ignorePatterns: ['gatsby-config.js', 'node_modules/'],
+  ignorePatterns: ["gatsby-config.js", "node_modules/"],
   rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-    'no-useless-constructor': 'off',
-    '@typescript-eslint/no-useless-constructor': 'error',
-    quotes: 'off',
-    '@typescript-eslint/quotes': [
-      2,
-      'single',
-      {
-        avoidEscape: true,
-      },
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-useless-constructor": "error",
+    "react/jsx-filename-extension": [
+      "error",
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] }
     ],
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'prettier/prettier': [
-      'error',
+    "import/extensions": ["error", "never", { json: "always", svg: "always" }],
+    quotes: "off",
+    "@typescript-eslint/quotes": [
+      2,
+      "single",
       {
-        trailingComma: 'es5',
+        avoidEscape: true
+      }
+    ],
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
         semi: false,
         singleQuote: true,
-        printWidth: 120,
-      },
-    ],
-  },
-}
+        printWidth: 120
+      }
+    ]
+  }
+};
